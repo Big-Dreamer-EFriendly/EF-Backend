462a0fce7c630bab9ac7e812d85f47370dbfff09
lapleDF <lap.le@digitalfortress.dev>
2024-02-26 08:49:07 +0700

    change-password

--- src/Routes/authRoutes.js
+++ src/Routes/authRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const authController = require('../Controller/authController');
-const UserController = require('../Controller/loginController');
 const verifyToken = require('../Middleware/authMiddleware.js');
 
 
@@ -12,7 +11,7 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
-router.post('/forgot-password', UserController.forgotPassword);
+router.post('/forgot-password', authController.forgotPassword);
 router.post('/change-password',verifyToken, authController.changePassword);
 router.post('/refreshToken', authController.refreshToken);
 
