43ee120299f1381285652f6e8c5b53d0e47a671d
lapleDF <lap.le@digitalfortress.dev>
2024-02-29 10:47:02 +0700

    feat/categories

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -3,13 +3,14 @@ const authRoutes = require('./authRoutes.js');
 const roomRoutes = require('./roomRoutes.js')
 const profileRoutes=require('./profileRoutes.js')
 const deviceRoomRoutes = require('./deviceRoomRoutes.js')
+const deviceRoutes = require('./deviceRoutes.js')
 
 const verifyToken = require('../Middleware/authMiddleware.js');
 
 
 function routes (app) {
     app.use('/auth',authRoutes);
-    app.use('/',verifyToken,roomRoutes,deviceRoomRoutes,profileRoutes);
+    app.use('/',verifyToken,roomRoutes,deviceRoomRoutes,profileRoutes,deviceRoutes);
     // app.use('/',);
 
 
