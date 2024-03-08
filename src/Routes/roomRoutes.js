a4930988f4abebe6d584cf34341d2e3046adcddf
ptho6452 <tho.phan24@student.passerellesnumeriques.org>
2024-03-09 01:06:58 +0700

    finish

--- src/Routes/roomRoutes.js
+++ src/Routes/roomRoutes.js
@@ -11,7 +11,7 @@ const router = express.Router();
 router.post('/rooms', roomController.createRoom);
 router.delete('/rooms/:id',roomController.deleteRoom)
 router.put('/rooms/:id',roomController.editRoom)
-router.get('/rooms12',roomController.getRoomsByUserId)
+router.get('/room',roomController.getRoomsByUserId)
 
 
 
