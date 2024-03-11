65065311e279e79ab3b991ada1cc1dc1bfd559aa
HuongPhanDev <153520763+HuongPhanDev@users.noreply.github.com>
2024-03-11 14:54:08 +0700

    Fix can not add room

--- src/Controller/roomController.js
+++ src/Controller/roomController.js
@@ -25,14 +25,13 @@ class RoomController {
     }
   }
   async createRoom(req, res) {
+    console.log(req.body);
+
     try {
       const { name, floor } = req.body;
-
       const { user_id } = req;
       const existingRoom = await Room.findOne({ name });
-      if (existingRoom) {
-  return res.status(400).json({ code: 400, message: 'Room already exists' });
-    }
+    
       const specialCharsRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
     if (specialCharsRegex.test(name) && !name.includes(' ') ||specialCharsRegex.test(name)) {
       return res.status(400).json({ code:400,message: 'Invalid characters in name' });
