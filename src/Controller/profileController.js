d297ac9b6f266ea8881c153241227540a03fe07f
ptho6452 <tho.phan24@student.passerellesnumeriques.org>
2024-03-15 14:31:01 +0700

    feat/profile

--- src/Controller/profileController.js
+++ src/Controller/profileController.js
@@ -17,6 +17,24 @@ async function updateUser(req, res) {
     res.status(500).json({code:500, message: 'Failed to update user information' });
   }
 }
+async function getUser(req, res) {
+  const {user_id} = req;
+
+  try {
+    const user = await userModels.findById({_id:user_id});
+
+    if (!user) {
+      return res.status(404).json({code:404, message: 'User not found' });
+    }
+
+    res.status(200).json({code:200, message: 'Get information successfully', user });
+  } catch (error) {
+    console.error('Internal:', error);
+    res.status(500).json({code:500, message: 'Internal' });
+  }
+}
+
 module.exports = {
-  updateUser
+  updateUser,
+  getUser
 };
