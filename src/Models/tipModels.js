640c99e94fa08de9aa4878b797cca24c61b1611e
ptho6452 <tho.phan24@student.passerellesnumeriques.org>
2024-03-18 01:34:25 +0700

    update

---
+++ src/Models/tipModels.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const tipSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  isRead: {
+    type: Boolean,
+    default:false,
+  },
+
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'users', 
+    required: true,
+  },
+});
+
+
+const Tip = mongoose.model('tips', tipSchema);
+
+module.exports = Tip;
