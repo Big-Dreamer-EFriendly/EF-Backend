ad4366e66854e6bc775c46ab4986390720509bbb
ptho6452 <tho.phan24@student.passerellesnumeriques.org>
2024-03-24 18:27:39 +0700

    feat/tips: Push Notifications

--- src/Routes/tipRoutes.js
+++ src/Routes/tipRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const tipController = require('../Controller/tipController');
 
 router.post('/tips/Month', tipController.CompareByMonth);
-router.post('/tip/week', tipController.CompareByWeek);
+router.post('/tips/week', tipController.CompareByWeek);
 router.post('/tips',tipController.addTips)
 router.get('/tips',tipController.getTipByUserId);
 router.put('/tips',tipController.updateStatusRead);
